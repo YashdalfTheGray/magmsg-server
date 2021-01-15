a84e2ca946522ae1505dd1b8226e8c2bc926605d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-15 09:05:14 -0800

    use the aws region construct instead of strings

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,3 +1,4 @@
+use rusoto_core::Region;
 use rusoto_dynamodb::DynamoDbClient;
 use rusoto_sts::StsClient;
 
@@ -6,7 +7,7 @@ pub fn get_creds(
     secret_access_key: String,
     role_arn: String,
     external_id: String,
-    region: String,
+    region: Region,
 ) {
     let client = StsClient::new(region);
 }
@@ -15,7 +16,7 @@ pub fn get_dynamo_client(
     access_key_id: String,
     secret_access_key: String,
     session_token: String,
-    region: String,
+    region: Region,
 ) {
-    let client = DynamoDbClient::new(Region::UsEast1);
+    let client = DynamoDbClient::new(region);
 }
