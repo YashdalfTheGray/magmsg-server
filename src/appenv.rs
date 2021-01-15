e9dc6c7fd16a9d27154404ab6bf6a85963273bd7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-15 09:17:37 -0800

    added a thing to get the aws region defaulting to us-east-2

--- src/appenv.rs
+++ src/appenv.rs
@@ -1,8 +1,17 @@
 use std::env;
 
+use rusoto_core::Region;
+
 pub fn port() -> u16 {
     match env::var("PORT") {
         Ok(p) => p.parse::<u16>().unwrap_or(8080),
         Err(_e) => 8080,
+    }
+}
+
+pub fn region() -> Region {
+    match env::var("AWS_REGION") {
+        Ok(r) => r.parse::<Region>().unwrap_or(Region::UsEast2),
+        Err(_e) => Region::UsEast2,
     }
 }
