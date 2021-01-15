d054844724c1cd40d7db1e43ab2f41bd92476ee3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-14 23:29:53 -0800

    added some more sdk functions to implement

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,11 +1,21 @@
+use rusoto_dynamodb::DynamoDbClient;
 use rusoto_sts::StsClient;
 
 pub fn get_creds(
-    access_key_id: string,
-    secret_access_key: string,
-    role_arn: string,
-    external_id: string,
-    region: string,
+    access_key_id: String,
+    secret_access_key: String,
+    role_arn: String,
+    external_id: String,
+    region: String,
 ) {
     let client = StsClient::new(region);
+}
+
+pub fn get_dynamo_client(
+    access_key_id: String,
+    secret_access_key: String,
+    session_token: String,
+    region: String,
+) {
+    let client = DynamoDbClient::new(Region::UsEast1);
 }
