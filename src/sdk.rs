842dbc2bca07c6fc2b9d33b6c26f7e0e8b755224
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-16 01:55:20 -0800

    added a function that gets the current time in milliseconds

--- src/sdk.rs
+++ src/sdk.rs
@@ -28,3 +28,12 @@ pub fn get_dynamo_client(
 ) {
     let client = DynamoDbClient::new(region);
 }
+
+fn get_time_in_millis() -> u128 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    since_the_epoch.as_millis()
+}
