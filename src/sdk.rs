9a1dc4c9b10a7a654cfed9bc68c1e3756a8e5e76
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-16 01:43:34 -0800

    got an sts client working

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,15 +1,23 @@
+use std::sync::Arc;
+
 use rusoto_core::Region;
+use rusoto_credential::StaticProvider;
 use rusoto_dynamodb::DynamoDbClient;
 use rusoto_sts::StsClient;
 
 pub fn get_creds(
-    access_key_id: String,
-    secret_access_key: String,
     role_arn: String,
     external_id: String,
     region: Region,
+    user_creds_provider: Option<StaticProvider>,
 ) {
-    let client = StsClient::new(region);
+    let credentials_provider = match (user_creds_provider) {
+        Some(cp) => cp,
+        None => crate::appenv::assume_role_user_creds(),
+    };
+    let http_client = rusoto_core::HttpClient::new().unwrap();
+    let arced_client = Arc::new(http_client);
+    let client = StsClient::new_with(arced_client, credentials_provider, region);
 }
 
 pub fn get_dynamo_client(
