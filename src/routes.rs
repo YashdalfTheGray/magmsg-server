016d139cf34cce79328665192c000f7ac444c37e
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-20 04:42:54 -0800

    wired up the credentials provider from the rocket state

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,8 @@
-use rocket::http::Status;
+use rocket::{http::Status, State};
 use rocket_contrib::json::JsonValue;
-use rusoto_dynamodb::DynamoDbClient;
+use rusoto_credential::AutoRefreshingProvider;
+
+use crate::sdk::CustomStsProvider;
 
 #[get("/")]
 pub fn index() -> &'static str {
@@ -15,11 +17,14 @@ pub fn api_index() -> JsonValue {
 }
 
 #[get("/api/messages")]
-pub fn get_all_messages() -> JsonValue {
-    let client = DynamoDbClient::new(crate::appenv::region());
+pub fn get_all_messages(
+    creds_provider: State<AutoRefreshingProvider<CustomStsProvider>>,
+) -> JsonValue {
+    let region = crate::appenv::region();
+    let client = crate::sdk::get_dynamo_client((*creds_provider.get_ref()).clone(), region);
     let messages = crate::dal::get_all_messages(
         client,
-        "messages".to_string(),
+        crate::appenv::table_name(),
         Some("createdAt,content".to_string()),
     );
     json!([])
