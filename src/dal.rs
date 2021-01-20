231867593b3e9420a48c123e8c8675f45006e5cd
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-20 05:10:24 -0800

    unwrap the future and options and all that to get the hashmap

--- src/dal.rs
+++ src/dal.rs
@@ -1,9 +1,17 @@
-use rusoto_dynamodb::{DynamoDbClient, ListTablesInput};
+use std::collections::HashMap;
 
-pub fn get_all_messages(
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, ScanInput};
+
+pub async fn get_all_messages(
     client: DynamoDbClient,
     table_name: String,
     fields_to_get_csv: Option<String>,
-) {
-    let list_tables_input = ListTablesInput::default();
+) -> Vec<HashMap<String, AttributeValue>> {
+    let scan_input = ScanInput {
+        table_name: table_name,
+        projection_expression: fields_to_get_csv,
+        ..Default::default()
+    };
+
+    client.scan(scan_input).await.unwrap().items.unwrap()
 }
