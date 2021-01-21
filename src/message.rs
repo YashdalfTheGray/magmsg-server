fb6c5337bc8a4c139a833a998c4f94135a18a977
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-21 01:02:14 -0800

    set up the from and into traits for messages

--- src/message.rs
+++ src/message.rs
@@ -1,7 +1,30 @@
+use std::collections::HashMap;
+
+use rusoto_dynamodb::AttributeValue;
+
 #[derive(Debug, Clone)]
 pub struct Message {
     message_id: String,
     created_at: u64,
     content: String,
     created_by: String,
+}
+
+impl From<HashMap<String, AttributeValue>> for Message {
+    fn from(dynamo_item: HashMap<String, AttributeValue>) -> Self {
+        Message {
+            message_id: Default::default(),
+            created_at: Default::default(),
+            content: Default::default(),
+            created_by: Default::default(),
+        }
+    }
+}
+
+impl Into<HashMap<String, AttributeValue>> for Message {
+    fn into(self) -> HashMap<String, AttributeValue> {
+        let result = HashMap::<String, AttributeValue>::new();
+
+        result
+    }
 }
