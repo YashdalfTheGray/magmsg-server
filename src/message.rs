c2a72860361f9c36aec63b38ddf9904d74d7f214
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-22 05:09:37 -0800

    wrote a bunch of code that actually fucking works

--- src/message.rs
+++ src/message.rs
@@ -2,18 +2,6 @@ use std::collections::HashMap;
 
 use rusoto_dynamodb::AttributeValue;
 
-enum AttributeDataType {
-    B,
-    BS,
-    L,
-    M,
-    N,
-    NS,
-    NULL,
-    S,
-    SS,
-}
-
 #[derive(Debug, Clone)]
 pub struct Message {
     message_id: String,
@@ -25,10 +13,28 @@ pub struct Message {
 impl From<HashMap<String, AttributeValue>> for Message {
     fn from(dynamo_item: HashMap<String, AttributeValue>) -> Self {
         Message {
-            message_id: Default::default(),
-            created_at: Default::default(),
-            content: Default::default(),
-            created_by: Default::default(),
+            message_id: dynamo_item.get("messageId").map_or_else(
+                || String::default(),
+                |val| val.s.clone().unwrap_or_default(),
+            ),
+            created_at: dynamo_item.get("createdAt").map_or_else(
+                || u64::default(),
+                |val| {
+                    val.n
+                        .clone()
+                        .unwrap_or_default()
+                        .parse::<u64>()
+                        .unwrap_or_default()
+                },
+            ),
+            content: dynamo_item.get("content").map_or_else(
+                || String::default(),
+                |val| val.s.clone().unwrap_or_default(),
+            ),
+            created_by: dynamo_item.get("createdBy").map_or_else(
+                || String::default(),
+                |val| val.s.clone().unwrap_or_default(),
+            ),
         }
     }
 }
@@ -38,19 +44,5 @@ impl Into<HashMap<String, AttributeValue>> for Message {
         let result = HashMap::<String, AttributeValue>::new();
 
         result
-    }
-}
-
-fn get_data_from<T>(attr: AttributeValue, datatype: AttributeDataType) -> T {
-    match datatype {
-        B => attr.b.unwrap_or_default(),
-        BS => attr.bs.unwrap_or_default(),
-        L => attr.l.unwrap_or_default(),
-        M => attr.m.unwrap_or_default(),
-        N => attr.n.unwrap_or_default(),
-        NS => attr.ns.unwrap_or_default(),
-        NULL => attr.null.unwrap_or_default(),
-        S => attr.s.unwrap_or_default(),
-        SS => attr.ss.unwrap_or_default(),
     }
 }
