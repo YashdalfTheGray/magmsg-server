53edd5ec1977fe75befc85351fc990480c9e93c6
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-22 04:39:59 -0800

    how is typescript's type system smarter than this motherfucking language?

--- src/message.rs
+++ src/message.rs
@@ -40,3 +40,17 @@ impl Into<HashMap<String, AttributeValue>> for Message {
         result
     }
 }
+
+fn get_data_from<T>(attr: AttributeValue, datatype: AttributeDataType) -> T {
+    match datatype {
+        B => attr.b.unwrap_or_default(),
+        BS => attr.bs.unwrap_or_default(),
+        L => attr.l.unwrap_or_default(),
+        M => attr.m.unwrap_or_default(),
+        N => attr.n.unwrap_or_default(),
+        NS => attr.ns.unwrap_or_default(),
+        NULL => attr.null.unwrap_or_default(),
+        S => attr.s.unwrap_or_default(),
+        SS => attr.ss.unwrap_or_default(),
+    }
+}
