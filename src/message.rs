ea3cdd6b99798d4942f98ad7015cb5a5d9888197
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-24 11:58:38 -0800

    changed constant fields to be the way they should be written out

--- src/message.rs
+++ src/message.rs
@@ -3,10 +3,10 @@ use std::collections::HashMap;
 use rusoto_dynamodb::AttributeValue;
 use uuid::Uuid;
 
-const id_field: &str = "messageId";
-const created_at_field: &str = "createdAt";
-const content_field: &str = "content";
-const created_by_field: &str = "createdBy";
+const ID_FIELD: &str = "messageId";
+const CREATED_AT_FIELD: &str = "createdAt";
+const CONTENT_FIELD: &str = "content";
+const CREATED_BY_FIELD: &str = "createdBy";
 
 #[derive(Debug, Clone)]
 pub struct Message {
@@ -30,13 +30,13 @@ impl Message {
 impl From<HashMap<String, AttributeValue>> for Message {
     fn from(dynamo_item: HashMap<String, AttributeValue>) -> Self {
         Message {
-            message_id: crate::utils::get_string_from_attribute_value(dynamo_item.get(id_field)),
+            message_id: crate::utils::get_string_from_attribute_value(dynamo_item.get(ID_FIELD)),
             created_at: crate::utils::get_number_from_attribute_value(
-                dynamo_item.get(created_at_field),
+                dynamo_item.get(CREATED_AT_FIELD),
             ),
-            content: crate::utils::get_string_from_attribute_value(dynamo_item.get(content_field)),
+            content: crate::utils::get_string_from_attribute_value(dynamo_item.get(CONTENT_FIELD)),
             created_by: crate::utils::get_string_from_attribute_value(
-                dynamo_item.get(created_by_field),
+                dynamo_item.get(CREATED_BY_FIELD),
             ),
         }
     }
@@ -47,19 +47,19 @@ impl Into<HashMap<String, AttributeValue>> for Message {
         let mut result = HashMap::<String, AttributeValue>::new();
 
         result.insert(
-            id_field.to_string(),
+            ID_FIELD.to_string(),
             crate::utils::wrap_string_in_attribute_value(self.message_id),
         );
         result.insert(
-            created_at_field.to_string(),
+            CREATED_AT_FIELD.to_string(),
             crate::utils::wrap_string_in_attribute_value(self.created_at.to_string()),
         );
         result.insert(
-            content_field.to_string(),
+            CONTENT_FIELD.to_string(),
             crate::utils::wrap_string_in_attribute_value(self.content),
         );
         result.insert(
-            created_by_field.to_string(),
+            CREATED_BY_FIELD.to_string(),
             crate::utils::wrap_string_in_attribute_value(self.created_by),
         );
 
