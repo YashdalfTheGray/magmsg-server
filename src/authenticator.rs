bbbbcf535ee05b1d22b708a9ef3feaea3e1339eb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-29 12:10:56 -0800

    added a todo for implementing fromrequest for authenticator

--- src/authenticator.rs
+++ src/authenticator.rs
@@ -1,5 +1,16 @@
-use rocket::request::FromRequest;
+use rocket::{
+    request::{FromRequest, Outcome},
+    Request,
+};
 
+#[derive(Debug)]
+enum AuthError {
+    BadFormat,
+    Missing,
+    Invalid,
+}
+
+#[derive(Debug)]
 struct Authenticator {
     api_key: String,
 }
@@ -7,5 +18,13 @@ struct Authenticator {
 impl Authenticator {
     pub fn is_valid(&self) -> bool {
         self.api_key == crate::appenv::user_access_token()
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Authenticator {
+    type Error = AuthError;
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        todo!()
     }
 }
