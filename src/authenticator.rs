e19ac4392466526c80a59074daeb0eeef844e380
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-30 12:03:48 -0800

    added a finished (hopefully) implementation of the auth checker

--- src/authenticator.rs
+++ src/authenticator.rs
@@ -1,4 +1,5 @@
 use rocket::{
+    http::Status,
     request::{FromRequest, Outcome},
     Request,
 };
@@ -23,6 +24,30 @@ impl<'a, 'r> FromRequest<'a, 'r> for Authenticator {
     type Error = AuthError;
 
     fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
-        todo!()
+        let header_values = request.headers().get("x-api-key").collect::<Vec<_>>();
+
+        match header_values.len() {
+            0 => Outcome::Failure((Status::Unauthorized, AuthError::Missing)),
+            1 => {
+                let parts = header_values[0]
+                    .split_ascii_whitespace()
+                    .collect::<Vec<_>>();
+
+                match parts.len() {
+                    0 | 1 => Outcome::Failure((Status::BadRequest, AuthError::Malformed)),
+                    2 if parts[0] != "Bearer" => {
+                        Outcome::Failure((Status::BadRequest, AuthError::Malformed))
+                    }
+                    2 if parts[0] == "Bearer" && is_valid(parts[1].to_string()) => {
+                        Outcome::Success(Authenticator {
+                            api_key: parts[1].to_string(),
+                        })
+                    }
+                    2 => Outcome::Failure((Status::BadRequest, AuthError::Malformed)),
+                    _ => Outcome::Failure((Status::BadRequest, AuthError::Malformed)),
+                }
+            }
+            _ => Outcome::Failure((Status::BadRequest, AuthError::Invalid)),
+        }
     }
 }
