003538e01d3d8cecbd055ab782f70d77e2466a34
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-02 01:28:36 -0800

    oh man, we really didn't need all that code, cleaned up

--- src/request_id.rs
+++ src/request_id.rs
@@ -1,26 +1,16 @@
 use rocket::{
     fairing::{Fairing, Info, Kind},
-    http::Header,
-    request::{self, FromRequest},
     Data, Request, Response,
 };
 
+use uuid::Uuid;
+
 #[derive(Debug)]
-pub struct RequestId(pub String);
+pub struct RequestId {}
 
-impl RequestId {
-    pub fn nil() -> RequestId {
-        RequestId(uuid::Uuid::nil().to_string())
-    }
-}
-
-impl<'a, 'r> FromRequest<'a, 'r> for &'a RequestId {
-    type Error = ();
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        request::Outcome::Success(
-            request.local_cache(|| RequestId(uuid::Uuid::new_v4().to_string())),
-        )
+impl Default for RequestId {
+    fn default() -> Self {
+        RequestId {}
     }
 }
 
@@ -33,11 +23,11 @@ impl Fairing for RequestId {
     }
 
     fn on_request(&self, request: &mut Request, _: &Data) {
-        request.local_cache(|| RequestId(uuid::Uuid::new_v4().to_string()));
+        request.local_cache(|| Uuid::new_v4().to_string());
     }
 
     fn on_response(&self, request: &Request, response: &mut Response) {
-        let id = request.local_cache(|| RequestId::nil());
-        response.set_raw_header("X-Request-ID", id.0.clone());
+        let id = request.local_cache(|| Uuid::nil().to_string());
+        response.set_raw_header("X-Request-ID", id.clone());
     }
 }
