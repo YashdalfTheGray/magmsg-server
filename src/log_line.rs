b05f5de44a8d258cc1d9d42713bf64c5d77e0316
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-03 02:53:15 -0800

    started implementing from<request> for log line

--- src/log_line.rs
+++ src/log_line.rs
@@ -1,4 +1,5 @@
 use chrono::{offset::Utc, DateTime, Duration};
+use rocket::Request;
 
 #[derive(Debug)]
 pub struct LogLine {
@@ -9,4 +10,18 @@ pub struct LogLine {
     received_at: DateTime<Utc>,
     responded_at: DateTime<Utc>,
     duration: Duration,
+}
+
+impl From<Request<'_>> for LogLine {
+    fn from(req: Request) -> Self {
+        LogLine {
+            request_id: String::from("some string"),
+            path: String::from("some string"),
+            method: String::from("some string"),
+            client_addr: String::from("some string"),
+            received_at: Utc::now(),
+            responded_at: Utc::now(),
+            duration: Duration::seconds(10),
+        }
+    }
 }
