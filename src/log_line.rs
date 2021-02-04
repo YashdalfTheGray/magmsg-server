69f1b4628770b5480e6fcea2f0e8f0ac4b25957f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-04 01:31:23 -0800

    filled out the log line from request implementation

--- src/log_line.rs
+++ src/log_line.rs
@@ -1,12 +1,14 @@
+use std::net::IpAddr;
+
 use chrono::{offset::Utc, DateTime, Duration};
-use rocket::Request;
+use rocket::{http::Method, Request};
 
 #[derive(Debug)]
 pub struct LogLine {
     request_id: String,
     path: String,
-    method: String,
-    client_addr: String,
+    method: Method,
+    client_addr: Option<IpAddr>,
     received_at: DateTime<Utc>,
     responded_at: DateTime<Utc>,
     duration: Duration,
@@ -15,13 +17,13 @@ pub struct LogLine {
 impl From<Request<'_>> for LogLine {
     fn from(req: Request) -> Self {
         LogLine {
-            request_id: String::from("some string"),
-            path: String::from("some string"),
-            method: String::from("some string"),
-            client_addr: String::from("some string"),
+            request_id: req.local_cache(|| uuid::Uuid::nil().to_string()).clone(),
+            path: req.uri().to_string(),
+            method: req.method(),
+            client_addr: req.client_ip(),
             received_at: Utc::now(),
             responded_at: Utc::now(),
-            duration: Duration::seconds(10),
+            duration: Duration::seconds(0),
         }
     }
 }
