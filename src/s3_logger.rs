bd789cc00b102837b0a53266b1247be286b59762
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-13 12:43:01 -0800

    basic logs collection things for now

--- src/s3_logger.rs
+++ src/s3_logger.rs
@@ -1,9 +1,17 @@
-use std::fs::File;
-
-use rusoto_s3::S3Client;
+use crate::log_line::LogLine;
 
 pub struct S3Logger {
-    file: Option<File>,
-    debug: bool,
-    s3_client: S3Client,
+    log_lines_cache: Vec<LogLine>,
+}
+
+impl S3Logger {
+    pub fn new() -> S3Logger {
+        S3Logger {
+            log_lines_cache: vec![],
+        }
+    }
+
+    pub fn log_request(&mut self, line: LogLine) {
+        self.log_lines_cache.push(line);
+    }
 }
