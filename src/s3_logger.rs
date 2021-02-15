d9b4a0d316767f8cf0b23b2b996fdbe5087b67a1
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-15 10:31:36 -0800

    added logic to collect the logs and print them to a file

--- src/s3_logger.rs
+++ src/s3_logger.rs
@@ -33,16 +33,23 @@ impl S3Logger {
 
     pub fn log_request(&mut self, line: LogLine) {
         self.log_lines_cache.push(line.clone());
+    }
+
+    pub fn publish_to_s3(&mut self) {
+        let s3client = sdk::get_s3_client(self.creds_provider.clone(), region());
         let mut file = OpenOptions::new()
             .append(true)
             .create(true)
             .open("logs/requests.log")
             .unwrap();
 
-        file.write((line.to_string() + "\n").as_bytes()).unwrap();
-    }
+        let chunk_of_logs = self
+            .log_lines_cache
+            .iter()
+            .map(|l| l.to_string())
+            .collect::<Vec<String>>()
+            .join("\n");
 
-    pub fn publish_to_s3(&mut self) {
-        let s3client = sdk::get_s3_client(self.creds_provider.clone(), region());
+        file.write((chunk_of_logs + "\n").as_bytes()).unwrap();
     }
 }
