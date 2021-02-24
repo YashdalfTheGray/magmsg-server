69453e9ce23d004b0f2e4bae33fa91588ec2c339
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-02-24 11:03:31 -0800

    missed a dot in the file name, we might need to look into log4rs

--- src/utils.rs
+++ src/utils.rs
@@ -89,7 +89,7 @@ pub fn configure_application_logging(path: String) -> Result<(), fern::InitError
                     ))
                 })
                 .level(log_level())
-                .chain(fern::DateBased::new(path, "%Y-%m-%d-%H")),
+                .chain(fern::DateBased::new(path, ".%Y-%m-%d-%H")),
         )
         .chain(
             fern::Dispatch::new()
